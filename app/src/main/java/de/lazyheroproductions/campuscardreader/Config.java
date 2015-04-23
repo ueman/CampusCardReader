ac7d8c313e9ada708b8f4a15413197eab64c8f79
ueman <j.uekoetter@web.de>
2015-04-23 20:01:03 +0200

    aksgzdbf

--- app/src/main/java/de/lazyheroproductions/campuscardreader/Config.java
+++ app/src/main/java/de/lazyheroproductions/campuscardreader/Config.java
@@ -6,10 +6,10 @@ package de.lazyheroproductions.campuscardreader;
 public class Config {
 
     //config stuff belongs here
-    public static final String NEXUS = "4FEF4F927C466A6DE35F2189F640FDF6";
-    public static final String MEMOPAD = "1D853E92C0D157C01C9AC1A5337D9CD9";
-    public static final String LIFETAB = "A18AD403544E81DF3ED30CC21C0FB702";
-    public static final String AD_UNIT_ID = "ca-app-pub-2722333505576951/4898658428";
+    public static final String NEXUS = "change me";
+    public static final String MEMOPAD = "change me";
+    public static final String LIFETAB = "change me";
+    public static final String AD_UNIT_ID = "change me";
 
     static final String PLAY_STORE_URI = "market://details?id=";
     static final String WEB_PLAY_STORE_URL = "http://play.google.com/store/apps/details?id=";
